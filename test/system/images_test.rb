e622b008e9ae5eef6190c7c084f20cd909894a81
Kuda <kuda@etapath.com>
2025-05-15 15:36:44 +0200

    fix system tests

--- test/system/images_test.rb
+++ test/system/images_test.rb
@@ -15,10 +15,11 @@ class ImagesTest < ApplicationSystemTestCase
     click_on "New image"
 
     fill_in "Name", with: @image.name
-    fill_in "Status", with: @image.status
+    find("input[type='file']").send_keys(Rails.root.join("test/fixtures/files/sample_picture.jpg"))
     click_on "Create Image"
 
     assert_text "Image was successfully created"
+
     click_on "Back"
   end
 
